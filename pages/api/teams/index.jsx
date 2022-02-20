059451633a8f551678d41b698634edb6b33d5979
Manuel <liproqq@gmail.com>
2022-02-20 21:17:25 +0100

    getCurrentTeams

---
+++ pages/api/teams/index.jsx
@@ -0,0 +1,26 @@
+import executeQuery, { sql2json } from 'lib/db';
+
+import { apiHandler } from 'helpers/api';
+
+export default apiHandler(handler);
+
+function handler(req, res) {
+  switch (req.method) {
+    case 'GET':
+      return getCurrentTeams();
+    default:
+      return res.status(200).end(`Internal Error in api/stats/game`)
+  }
+
+  async function getCurrentTeams() {
+    const teams = await executeQuery({
+      query:
+        `SELECT team_id, name, short, code FROM team
+        WHERE start<(SELECT season FROM season WHERE current=1) AND
+              end>(SELECT season FROM season WHERE current=1);`,
+      values: []
+    })
+
+    return res.status(200).json({ teams })
+  }
+}
