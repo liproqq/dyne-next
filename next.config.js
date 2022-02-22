34b0deaec6f105b7737d99d4df584c75fd25b234
Manuel <liproqq@gmail.com>
2022-02-22 23:19:10 +0100

    test api url

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            : '/api' // production api
     }
 }
